15a7b194ee200ba07324d6fea6febb64b6ef28f6
Rishabh bharota <rishabhbharota@gmail.com>
2021-07-17 16:08:08 +0530

    added sliding links

---
+++ script/moving_links.js
@@ -0,0 +1,19 @@
+
+// // Webinar on data science
+// // Hackerrank contest
+
+// function first() {
+//     document.getElementById('update-link').innerText = 'Webinar on Data Science';
+// }
+
+// function second() {
+//     document.getElementById('update-link').innerText = 'Hackerrank contest';
+// }
+
+// function third() {
+//     document.getElementById('update-link').innerText = 'Blog on SEO';
+// }
+
+// setInterval(first, 3000);
+// setInterval(second, 6000);
+// setInterval(third, 9000);
